7b7fc681280b185c1de9b124d92f587d9460ab34
Arthur <aarthur00ian@gmail.com>
2022-08-16 15:29:53 -0300

    methods added

--- app.py
+++ app.py
@@ -4,15 +4,18 @@ import json
 app = Flask(__name__)
 
 alunos = [
-    {'nome': 'Jhonatan',
+    {'id': 0,
+     'nome': 'Jhonatan',
      'cursos': ['matemática', 'física', 'química']
      },
 
-    {'nome': 'Arthur',
+    {'id': 1,
+     'nome': 'Arthur',
      'cursos': ['computação', 'física', 'matemática', 'química']
      },
 
-    {'nome':'Beatriz',
+    {'id':2,
+     'nome':'Beatriz',
      'cursos': ['história', 'redação', 'biologia']
      }
 ]
@@ -22,17 +25,42 @@ def homepage():
     return render_template('homepage.html')
 
 
-@app.route('/aluno/<int:id>', methods=['GET', 'PUT'])
+@app.route('/aluno/<int:id>', methods=['GET', 'PUT', 'DELETE'])
 def aluno(id):
     if request.method == 'GET':
-        aluno = alunos[id]
-        return jsonify({'aluno':aluno})
+        try:
+            response = alunos[id]
+        except IndexError:
+            message =  "Aluno de ID {} inexistente".format(id)
+            response = {"status": "erro", "message": message}
+        except Exception:
+            message = "Erro desconhecido"
+            response = {"status": "erro", "message": message}
+        finally:
+            return jsonify(response)
+
     elif request.method == 'PUT':
         dados = json.loads(request.data)
         alunos[id] = dados
         return jsonify(dados)
 
+    elif request.method == 'DELETE':
+        alunos.pop(id)
+        return jsonify({'status': 'sucesso', 'message': 'registro excluido'})
 
+
+
+
+@app.route('/aluno', methods=['POST', 'GET'])
+def registrar_aluno():
+    if request.method == 'POST':
+        dados = json.loads(request.data)
+        posicao = len(alunos)
+        dados['id'] = posicao
+        alunos.append(dados)
+        return jsonify({"status": "sucesso", "message":"registro inserido, aluno inserido no ID {}".format(alunos[len(alunos)-1]['id'])})
+    elif request.method == 'GET':
+        return jsonify(alunos)
 
 
 
